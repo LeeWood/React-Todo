43f6864f80cdc87ce47ba5893666b826537da7a8
aleesha-wood <spectrat@spectratart.com>
2019-11-01 19:11:40 -0400

    develed duplicate todo.css file

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,21 +3,21 @@ import Todo from './Todo';
 
 const TodoList = props => {
   return (
-    <>
-    <div className = "list">
-      <h2>Todo</h2>
-      {props.tasks.map(task => (
-        <Todo 
-          key={task.id}
-          task={task}
-          toggleCompleted={props.toggleCompleted}
-        />
-      ))}
+    <div className="list-conainer">
+      <div className = "list">
+        <h2>Todo</h2>
+        {props.tasks.map(task => (
+          <Todo 
+            key={task.id}
+            task={task}
+            toggleCompleted={props.toggleCompleted}
+          />
+        ))}
+      </div>
+      <div className="comp-list">
+        <h2>Completed</h2>
+      </div>
     </div>
-    <div className="comp-list">
-      <h2>Completed</h2>
-    </div>
-    </>
   )
 }
 
