8e52c646462ede3016ecf63871c9371b87c2af96
aleesha-wood <spectrat@spectratart.com>
2019-11-01 20:52:54 -0400

    completed items showing only on completed section

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -8,15 +8,26 @@ const TodoList = props => {
       <div className = "list">
         <h2 className="list-title">Todo</h2>
         {props.tasks.map(task => (
+          <div className="noncomp">
           <Todo 
             key={task.id}
             task={task}
             toggleCompleted={props.toggleCompleted}
           />
+          </div>
         ))}
       </div>
       <div className="comp-list">
         <h2 className="list-title">Completed</h2>
+        {props.tasks.map(task => (
+          <div className="comp">
+          <Todo 
+            key={task.id}
+            task={task}
+            toggleCompleted={props.toggleCompleted}
+          />
+          </div>
+        ))}
       </div>
     </div>
   )
